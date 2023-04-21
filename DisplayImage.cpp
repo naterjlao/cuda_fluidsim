998361b5d0fc714a306f874815f1b4011e0e04a1
nlao <lao.nathan@yahoo.com>
2023-04-21 15:16:50 -0400

    text example

--- DisplayImage.cpp
+++ DisplayImage.cpp
@@ -1,20 +1,25 @@
 #include <opencv2/opencv.hpp>
 #include <stdio.h>
-using namespace cv;
 int main(int argc, char** argv)
 {
     if (argc != 2) {
         printf("usage: DisplayImage.out <Image_Path>\n");
         return -1;
     }
-    Mat image;
-    image = imread(argv[1], 1);
+    cv::Mat image;
+    image = cv::imread(argv[1], 1);
     if (!image.data) {
         printf("No image data \n");
         return -1;
     }
-    namedWindow("Display Image", WINDOW_AUTOSIZE);
-    imshow("Display Image", image);
-    waitKey(0);
+
+    // Put Text
+    cv::Point text_coordinates(50,50);
+    cv::Scalar text_color(0,0,0);
+    cv::putText(image, "test", text_coordinates, cv::FONT_HERSHEY_SIMPLEX, 1.0, text_color, 3);
+
+    cv::namedWindow("Display Image", cv::WINDOW_AUTOSIZE);
+    cv::imshow("Display Image", image);
+    cv::waitKey(0);
     return 0;
 }
