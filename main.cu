5632dd1caab62eaf9f30685f75af6caefee73995
nlao <lao.nathan@yahoo.com>
2023-04-28 17:56:34 -0400

    display all

--- main.cu
+++ main.cu
@@ -25,7 +25,7 @@ static __host__ void initialize_p_field(float *data, const MatrixDim dim)
     {
         for (size_t x = x_lower; x < x_upper; x++)
         {
-#if 0
+#if 1
             data[matrix_index(x,y,dim,0)] = 2.0 * rand_norm_scalar();
             data[matrix_index(x,y,dim,1)] = 2.0;// * rand_norm_scalar();
 #else
@@ -36,21 +36,13 @@ static __host__ void initialize_p_field(float *data, const MatrixDim dim)
     }
 }
 
-static __host__ void initialize_bgr_field(unsigned int *data, const size_t nElements)
-{
-    // Set the alpha field to max for all pixels
-    // Note: little endian representation (A,R,G,B)
-    for (size_t idx = 0; idx < nElements; idx++)
-        data[idx] = 0xFF000000; 
-}
-
 int main()
 {
     const MatrixDim DIMENSIONS = {768, 768, 2};
     const size_t N_ELEMENTS = DIMENSIONS.x * DIMENSIONS.y;
     const size_t FIELD_SIZE = sizeof(float) * N_ELEMENTS * DIMENSIONS.vl;
     const size_t BGR_SIZE = sizeof(unsigned int) * N_ELEMENTS;
-    const float RDX = 512.0;
+    const float RDX = 256.0;
 
     // Simulation timestep
     const float TIMESTEP = 0.01;
@@ -78,38 +70,56 @@ int main()
     cudaMalloc(&d_dfield, FIELD_SIZE);
 
     // Setup host image matrix
-    unsigned int *h_bgr;
-    cudaMallocHost(&h_bgr, BGR_SIZE);
-    initialize_bgr_field(h_bgr, N_ELEMENTS);
+    unsigned int *h_vbgr;
+    unsigned int *h_pbgr;
+    unsigned int *h_dbgr;
+    cudaMallocHost(&h_vbgr, BGR_SIZE);
+    cudaMallocHost(&h_pbgr, BGR_SIZE);
+    cudaMallocHost(&h_dbgr, BGR_SIZE);
 
     // Setup device image matrix
-    unsigned int *d_bgr;
-    cudaMalloc(&d_bgr, BGR_SIZE);
-    cudaMemcpy(d_bgr, h_bgr, BGR_SIZE, cudaMemcpyHostToDevice);
+    unsigned int *d_vbgr;
+    unsigned int *d_pbgr;
+    unsigned int *d_dbgr;
+    cudaMalloc(&d_vbgr, BGR_SIZE);
+    cudaMalloc(&d_pbgr, BGR_SIZE);
+    cudaMalloc(&d_dbgr, BGR_SIZE);
 
-    cv::Mat image;
+    cv::Mat vimage;
+    cv::Mat pimage;
+    cv::Mat dimage;
+    cv::namedWindow("Velocity", cv::WINDOW_AUTOSIZE);
+    cv::namedWindow("Pressure", cv::WINDOW_AUTOSIZE);
+    cv::namedWindow("Divergence", cv::WINDOW_AUTOSIZE);
     size_t temp = 0;
     while (true)
     {
         cudaMemcpy(d_vfield, h_vfield, FIELD_SIZE, cudaMemcpyHostToDevice);
-        cudaMemcpy(d_vfield, d_vfield, FIELD_SIZE, cudaMemcpyDeviceToDevice);
-        
-        kernel_advect<<<DIM_GRID, DIM_BLOCK>>>(DIMENSIONS,d_vfield,d_vfield,RDX,TIMESTEP,0.8);
+        cudaMemset(d_pfield,0, FIELD_SIZE);
+
+        kernel_advect<<<DIM_GRID, DIM_BLOCK>>>(DIMENSIONS,d_vfield,d_vfield,RDX,TIMESTEP,0.999);
         kernel_divergence<<<DIM_GRID, DIM_BLOCK>>>(DIMENSIONS,d_vfield,d_dfield,RDX/2.0);
+        kernel_jacobi<<<DIM_GRID, DIM_BLOCK>>>(DIMENSIONS,d_pfield,d_dfield,d_pfield,-1.0,0.25,20);
 
-        //kernel_gradient<<<DIM_GRID, DIM_BLOCK>>>(d_vfield, d_bgr, DIMENSIONS);
-        kernel_gradient<<<DIM_GRID, DIM_BLOCK>>>(d_dfield, d_bgr, DIMENSIONS);
+        kernel_gradient<<<DIM_GRID, DIM_BLOCK>>>(d_vfield, d_vbgr, DIMENSIONS);
+        kernel_gradient<<<DIM_GRID, DIM_BLOCK>>>(d_pfield, d_pbgr, DIMENSIONS);
+        kernel_gradient<<<DIM_GRID, DIM_BLOCK>>>(d_dfield, d_dbgr, DIMENSIONS);
+        cudaMemcpy(h_vbgr, d_vbgr, BGR_SIZE, cudaMemcpyDeviceToHost);
+        cudaMemcpy(h_pbgr, d_pbgr, BGR_SIZE, cudaMemcpyDeviceToHost);
+        cudaMemcpy(h_dbgr, d_dbgr, BGR_SIZE, cudaMemcpyDeviceToHost);
+
         cudaMemcpy(h_vfield, d_vfield, FIELD_SIZE, cudaMemcpyDeviceToHost);
-        cudaMemcpy(h_bgr, d_bgr, BGR_SIZE, cudaMemcpyDeviceToHost);
-        cudaDeviceSynchronize();
+        //cudaDeviceSynchronize();
 
-        image = cv::Mat(DIMENSIONS.y, DIMENSIONS.x, CV_8UC4, (unsigned *)h_bgr);
-
-        cv::namedWindow("Display Image", cv::WINDOW_AUTOSIZE);
-        cv::imshow("Display Image", image);
+        vimage = cv::Mat(DIMENSIONS.y, DIMENSIONS.x, CV_8UC4, (unsigned *)h_vbgr);
+        pimage = cv::Mat(DIMENSIONS.y, DIMENSIONS.x, CV_8UC4, (unsigned *)h_pbgr);
+        dimage = cv::Mat(DIMENSIONS.y, DIMENSIONS.x, CV_8UC4, (unsigned *)h_dbgr);
+        cv::imshow("Velocity", vimage);
+        cv::imshow("Pressure", pimage);
+        cv::imshow("Divergence", dimage);
         cv::waitKey(FRAMERATE);
 
-        if (temp > 500)
+        if (temp > 100)
         {
             temp = 0;
             initialize_p_field(h_vfield, DIMENSIONS);
@@ -121,10 +131,14 @@ int main()
     }
 
     cudaFree(h_vfield);
-    cudaFree(h_bgr);
+    cudaFree(h_vbgr);
+    cudaFree(h_pbgr);
+    cudaFree(h_dbgr);
     cudaFree(d_vfield);
     cudaFree(d_pfield);
     cudaFree(d_dfield);
-    cudaFree(d_bgr);
+    cudaFree(d_vbgr);
+    cudaFree(d_pbgr);
+    cudaFree(d_dbgr);
     return 0;
 }
